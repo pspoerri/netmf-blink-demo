064cd4ce8ee1ca1508c6349eda5cf2aff166582c
Pascal Spörri <pascal@spoerri.me>
2014-03-23 16:01:08 +0100

    Initial commit

---
+++ Blink/Leds.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SPOT.Hardware;
+using Mountaineer.Netmf.Hardware;
+
+namespace Blink
+{
+    public static class Leds
+    {
+        public static OutputPort Red { get; private set; }
+        public static OutputPort Green { get; private set; }
+        public static OutputPort Blue { get; private set; }
+
+        static Leds()
+        {
+            Red = new OutputPort(OnboardIO.LedRed, false);
+            Green = new OutputPort(OnboardIO.LedGreen, false);
+            Blue = new OutputPort(OnboardIO.LedBlue, false);
+        }
+
+        public static void Toggle(this OutputPort port)
+        {
+            port.Write(!port.Read());
+        }
+    }
+}
---
+++ Blink/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace Blink
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Leds.Red.Toggle();
+                Thread.Sleep(500); // Sleep for 500ms
+            }
+        }
+
+    }
+}
